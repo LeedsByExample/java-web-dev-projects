f3db786cb3cf4a74a050160cea448c467f55d08c
LeedsByExample <leeda.minkin@yahoo.com>
2023-12-03 19:01:07 -0600

    Completed studio.

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -1,2 +1,17 @@
-package org.launchcode;public class Area {
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter circle radius: ");
+
+        double radius = input.nextDouble();
+
+        double area = Circle.getArea(radius);
+
+        System.out.println("Your circle area is: " + area);
+    }
 }
--- datatypes/datatypes-studio/src/main/java/org/launchcode/Circle.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Circle.java
@@ -1,2 +1,7 @@
-package org.launchcode;public class Circle {
+package org.launchcode;
+public class Circle {
+    public static Double getArea(Double radius) {
+        return Math.PI * Math.pow(radius, 2);
+    }
 }
+//do I not need a public static void main here??

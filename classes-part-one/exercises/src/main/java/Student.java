a8787da0fddf41ddb491cc12d8a549e19ebea748
LeedsByExample <leeda.minkin@yahoo.com>
2023-12-20 20:56:11 -0600

    Worked on exercise

--- classes-part-one/exercises/src/main/java/Student.java
+++ classes-part-one/exercises/src/main/java/Student.java
@@ -4,6 +4,14 @@ public class Student {
         private int numberOfCredits = 0;
         private double gpa = 0.0;
 
+        public String getName() {
+                return name; //why the fuck is my indent so excessive here
+        }
+
+        public void setName(String aName) {
+                name = aName; //when am i supposed to use aName vs. this.name
+        }
+
         // Drop your getters and setters below for the Student class.
         // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
 
